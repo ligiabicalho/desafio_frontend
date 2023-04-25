36a935364937966af87d345c739a53ea82e1ccf1
Lígia Bicalho <ligiabicalho@gmail.com>
2023-04-25 14:50:46 -0300

    feat: add components tkt-modules na homepage

--- trakto_tv/src/app/pages/home/tkt-module/tkt-module.component.ts
+++ trakto_tv/src/app/pages/home/tkt-module/tkt-module.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ModulesEdu } from 'src/app/interfaces';
 
 @Component({
   selector: 'app-tkt-module',
   templateUrl: './tkt-module.component.html',
   styleUrls: ['./tkt-module.component.css'],
 })
-export class TktModuleComponent {}
+export class TktModuleComponent {
+  @Input() module: ModulesEdu = {
+    name: '',
+    bar: '',
+    rota: '',
+    image: '',
+  };
+}
