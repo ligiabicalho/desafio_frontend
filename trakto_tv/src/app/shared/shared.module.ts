ddc44b09cb1be08158897be77df2422686a8940d
Lígia Bicalho <ligiabicalho@gmail.com>
2023-04-27 15:34:37 -0300

    feat: add RouterModule in SharedModule

--- trakto_tv/src/app/shared/shared.module.ts
+++ trakto_tv/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BannerComponent } from './banner/banner.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent, BannerComponent],
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, RouterModule],
   exports: [HeaderComponent, BannerComponent],
   providers: [],
 })
