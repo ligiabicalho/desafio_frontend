c552b0e1e69ec8384eb536cbd04cc9c07ce51769
Lígia Bicalho <ligiabicalho@gmail.com>
2023-04-27 14:17:36 -0300

    refactor: header com tema claro e escuro dark/light

--- trakto_tv/src/app/shared/header/header.component.ts
+++ trakto_tv/src/app/shared/header/header.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Input() themeDark = true;
+
+  transformDate() {
+    const date = new Date().toLocaleDateString();
+    const format = 'dd/MM/yyyy';
+    const locale = 'en-US';
+    const formattedDate = formatDate(date, format, locale);
+    return formattedDate;
+  }
+}
