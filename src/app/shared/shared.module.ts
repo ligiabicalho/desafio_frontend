1ab3da83980420cacb291a1d5691d8ad8939d7d6
Lígia Bicalho <ligiabicalho@gmail.com>
2023-04-28 15:35:15 -0300

    feat: cria Page In Progress

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BannerComponent } from './banner/banner.component';
 import { HeaderComponent } from './header/header.component';
+import { InProgressComponent } from './in-progress/in-progress.component';
 
 @NgModule({
-  declarations: [HeaderComponent, BannerComponent],
+  declarations: [HeaderComponent, BannerComponent, InProgressComponent],
   imports: [CommonModule, HttpClientModule, RouterModule],
-  exports: [HeaderComponent, BannerComponent],
+  exports: [HeaderComponent, BannerComponent, InProgressComponent],
   providers: [],
 })
 export class SharedModule {}
