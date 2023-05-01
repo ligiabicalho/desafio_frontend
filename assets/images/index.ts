f552558e7c260304c3c0958dd166aacf61289af2
Lígia Bicalho <ligiabicalho@gmail.com>
2023-05-01 09:57:07 -0300

    Auto-generated commit [ci skip]

---
+++ assets/images/index.ts
@@ -0,0 +1,39 @@
+export const iconsHeader = {
+  logo: {
+    dark: '../../../assets/images/dark/logo-edu.svg',
+    light: '../../../assets/images/light/logo-edu-light.svg',
+  },
+  logout: '../../../assets/images/light/logout.png',
+  calendar: '../../../assets/images/dark/icon-calendar.png',
+  line: '../../../assets/images/dark/line.png',
+  bell: '../../../assets/images/dark/icon-notification.png',
+  avatar: {
+    img: '../../../assets/images/dark/avatar.png',
+    arrow: '../../../assets/images/dark/icon-arrow.png',
+  },
+  group: '../../../assets/images/light/right.png',
+};
+export const iconsGrid = {
+  arrow_left: '../../../../assets/images/course/arrow-left.svg',
+  arrow_right: '../../../../assets/images/course/arrow-right.png',
+  ivideo: '../../../../assets/images/course/icon-video.png',
+  videos: '../../../../../assets/images/course/videos.png',
+};
+export const bgCourse = {
+  img: '',
+};
+
+export const bannerCourse = {
+  src: {
+    background: '',
+    person: '../../../../assets/images/banner/person.png',
+    name: '../../../../assets/images/banner/TiagoMochileiro.svg',
+    arrow: '../../../../assets/images/banner/arrow.png',
+  },
+  alt: {
+    background: '',
+    person: 'Foto do Tiago Mochileiro',
+    name: 'Nome Tiago Mochileiro',
+    arrow: 'Seta ligando nome a foto',
+  },
+};
